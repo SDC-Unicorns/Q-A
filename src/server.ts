e8c220dd8d17b63bb1647b39bcb40156535757ac
ahusu <su.aust@gmail.com>
2023-07-17 13:50:11 -0700

    Created question post endpoint.  havnet tested

--- src/server.ts
+++ src/server.ts
@@ -16,18 +16,9 @@ type postReq = { params: {} }
 
 app.get('/qa/questions/', async (req: { query: { product_id: Number, page: Number, count: Number } }, res) => {
   let prod_id = req.query.product_id;
-
   let page = req.query.page;
   let count = req.query.count;
-  // let qprom = db.query(`SELECT * FROM questions WHERE product_id = ${prod_id} LIMIT ${count}`);
-  // let questions = await qprom
-  // console.log('this is the questions: ',questions)
 
-  // let aprom = db.query(`SELECT * FROM answers WHERE question_id IN (${questions.map((q:{id:Number})=>q.id)})`)
-  // let answers = await aprom;
-  // console.log('this is the answers:', answers)
-
-  //do this with a join table???
   let questions = await db.query(`
   EXPLAIN ANALYZE
   SELECT JSON_BUILD_OBJECT
@@ -50,34 +41,48 @@ app.get('/qa/questions/', async (req: { query: { product_id: Number, page: Numbe
   )
   FROM questions LEFT JOIN answers ON questions.id = answers.question_id WHERE questions.product_id = ?
   GROUP BY questions.id;
-  `, prod_id)
+  `, prod_id);
   console.log(questions)
   res.send(questions);
 });
 
-app.post('qa/questions/', (req, res) => {
+app.post('qa/questions/', async (req, res) => {
   //check auth?? (should this be middleware?)
   //write to DB
+  let { body: questionBody, name: questionName, email: questionEmail, product_id: productId } = req.body;
 
+  const questionQuery = `
+  INSERT INTO questions (product_id, body, date_written, asker_name, asker_email, reported, helpful)
+  VALUES ($1, $2, NOW(), $3, $4, false, 0)
+  RETURNING id
+`;
+try {
+  await db.one(questionQuery, [productId, questionBody, questionName, questionEmail]);
+  res.send('Question created successfully');
+} catch (err) {
+  res.send('Error writing new question')
+}
 
-  app.post('qa/answers/', async (req, res) => {
-    let { body: answerBody, name: username, email, photos } = req.body;
-    let questionId = req.query.question_id
+})
 
-    const answerQuery: QueryParam = `
+app.post('qa/answers/', async (req, res) => {
+  let { body: answerBody, name: username, email, photos } = req.body;
+  let questionId = req.query.question_id
+
+  const answerQuery: QueryParam = `
     INSERT INTO answers (question_id, body, date_written, answerer_name, answerer_email)
     VALUES ($1, $2, NOW(), $3, $4)
     RETURNING id
   `;
-    try {
-      let answerId = await db.one(answerQuery, [questionId, answerBody, username, email]);
-      res.send('Successfully written new Answer');
-    } catch (err: any) {
-      res.send('Error writing new answer');
-    }
+  try {
+    let answerId = await db.one(answerQuery, [questionId, answerBody, username, email]);
+    res.send('Successfully written new Answer');
+  } catch (err: any) {
+    res.send('Error writing new answer');
+  }
 
-  })
 })
+
 
 app.put('qa/questions', (req: putReq, res) => {
 
