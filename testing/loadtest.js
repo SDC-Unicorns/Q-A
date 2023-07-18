8cad07df871dff6a1934a6f2b7ddbad942285a74
ahusu <su.aust@gmail.com>
2023-07-17 18:59:40 -0700

    updated load testing to be randomly generating bot 10% of possible product_ids

--- testing/loadtest.js
+++ testing/loadtest.js
@@ -3,13 +3,15 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '10s', target: 50 },  // simulate ramp-up of traffic from 1 to 50 users over 10 seconds
-    { duration: '30s', target: 50 },  // stay at 50 users for 30 seconds
+    { duration: '10s', target: 3000 },  // simulate ramp-up of traffic from 1 to 50 users over 10 seconds
+    { duration: '30s', target: 3000 },  // stay at 50 users for 30 seconds
     { duration: '10s', target: 0 },   // ramp-down to 0 users over the last 10 seconds
   ],
 };
 
 export default function () {
-  let res = http.get('http://localhost:9000/qa/questions?product_id=40343&page=1&count=10');
+  let tot = 3518963;
+  let randomNum = Math.floor(0.9 * tot + Math.random(0.1 * tot))
+  let res = http.get(`http://localhost:9000/qa/questions?product_id=${randomNum}&page=1&count=10`);
   sleep(1);
 }
